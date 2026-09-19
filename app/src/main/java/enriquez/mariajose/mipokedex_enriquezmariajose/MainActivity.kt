b1d5a17665c6143aa09f4a91cba704b30c825591
Majo8805 <maria.enriquez252337@potros.itson.edu.mx>
2026-09-19 16:29:46 -0700

    Interfaz pokedex

--- app/src/main/java/enriquez/mariajose/mipokedex_enriquezmariajose/MainActivity.kt
+++ app/src/main/java/enriquez/mariajose/mipokedex_enriquezmariajose/MainActivity.kt
@@ -1,6 +1,8 @@
 package enriquez.mariajose.mipokedex_enriquezmariajose
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
